ce2f2abb39d620741d6292403a2af2e44e42ac0c
cameronmcnz <you@example.com>
2024-06-15 08:45:07 -0400

    'walk'

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 k.loadSprite("spritesheet", "./spritesheet.png"), {
   sliceX: 39, 
   sliceY: 31,
-  anims:{
+  anims: {
     "idle-down": 936,
+    "walk-down": [from: 936, to: 939, loop: true, speed: 8],
   }
 }
