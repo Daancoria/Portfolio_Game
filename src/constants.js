0a03e6703a6ec274f161525e2225a1fa45b1ecef
cameronmcnz <you@example.com>
2024-06-25 09:40:57 -0400

    'c'

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like HTML and Python. Anyway regardless of the language, I just like programming.
+    I made this of games in that language. I also like HTML and Python. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://https://github.com/sicarius777" target="_blank">Github</a>!`,
 
   "cs-degree": `This is my Certification. I hung it on the wall because I'm proud of it. It was a very comprehensive program I think it gave me a good foundation.`,
