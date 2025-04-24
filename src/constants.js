18bdb0b0937e31fa1f83130fb53d8d81266a66c7
Daniel Coria <daancoria@gmail.com>
2025-04-24 14:43:56 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -17,7 +17,7 @@ export const dialogueData = {
   bed: `This where I sleep. Don't mind the mess, I just woke up!`,
   
   resume: `This is my desk and on it is my resume. <a href="https://github.com/Daancoria/resume/blob/main/TResume.pdf" target="_blank">Check it out?</a>
-  Contact me at daancoria@mail.com or on my LinkedIn <a href="https://www.linkedin.com/in/daniel-anievas-coria-18a613354/" target="_blank">here</a> if you have any interesting job opportunities!`,
+  Contact me at daancoria@gmail.com or on my LinkedIn <a href="https://www.linkedin.com/in/daniel-anievas-coria-18a613354/" target="_blank">here</a> if you have any interesting job opportunities!`,
   
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.
