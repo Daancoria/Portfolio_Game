2973c28cf29a0180de15d92120924185c3e2e84b
Daniel Coria <daancoria@gmail.com>
2025-05-02 10:45:08 -0500

    Update constants.js
    Added links to certifications and badge

--- src/constants.js
+++ src/constants.js
@@ -5,27 +5,28 @@ export const dialogueData = {
   My portfolio includes multiplayer games, educational simulations, and interactive applications.
   In addition to my work as a developer, I have a background in professional translation and interpretation across English, Spanish, French, and Italian. 
   This linguistic expertise has been especially valuable in projects involving localization, accessibility, and education.
-  Whether I’m working on code, content, or communication—I enjoy solving complex problems and creating engaging, user-focused experiences.  
+  Whether I’m working on code, content, or communication, I enjoy solving complex problems and creating engaging, user-focused experiences.  
   You can explore some of my work on <a href="https://github.com/Daancoria" target="_blank">GitHub</a>.`,
 
-  "cs-degree": `This is my Coding Temple certification, and I’m proud to have it up on the wall. The program was immersive and project-driven, giving me a strong foundation in full-stack development. It’s where I honed my skills and built the groundwork for the projects I’m working on today.
-  <a href="https://www.comingsoon.com" target="_blank">Link</a>`,
+  "cs-degree": `These are my Coding Temple certifications, and I’m proud to have them up on the wall. The program was immersive and project-driven, giving me a strong foundation in full-stack development. It’s where I honed my skills and built the groundwork for the projects I’m working on today.
+  <a href="https://www.credential.net/8daf600a-4c18-480d-9c90-5f8d33fb1675" target="_blank">Link for Certificate of Completion</a>,
+  <a href="https://www.credential.net/8daf600a-4c18-480d-9c90-5f8d33fb1675" target="_blank">Link for Frontend Specialist Badge</a>`,
   
-  "sofa-table": `That's my sofa. I like to relax here and binge eat while scrolling or just taking a power nap.`,
+  "sofa-table": `That's my sofa. I like to relax here and read on my Kindle or take a power nap.`,
 
   tv: `That's my TV. I've been watching a lot of Netflix and Hulu lately. I love watching shows like "South Park" and "Bordertown".
-  I also enjoy video games, especially RPGs. I love playing Pokemon and I have most of the games.`,
+  I also enjoy video games, especially RPGs. I love playing Pokémon and have most of the games.`,
   
-  bed: `This where I sleep. Don't mind the mess, I just woke up!`,
+  bed: `This is where I sleep. Don't mind the mess, I just woke up!`,
   
   resume: `This is my desk and on it is my resume. <a href="https://github.com/Daancoria/resume/blob/main/TResume.pdf" target="_blank">Check it out?</a>
   Contact me at daancoria@gmail.com or on my LinkedIn <a href="https://www.linkedin.com/in/daniel-anievas-coria-18a613354/" target="_blank">here</a> if you have any interesting job opportunities!`,
   
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.
-  I have more games and projects on my personal website <a href=http://daancoria.github.io/ target="_blank">here</a>.`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within the canvas.
+  I have more games and projects on my website <a href=http://daancoria.github.io/ target="_blank">here</a>.`,
   
-  library: `There are a wide variety of books from novels to fiction, I personally love sci-fi novels, mythology, & anything from fantasy to horror.
+  library: `There are a wide variety of books from novels to fiction, I love sci-fi novels, mythology, & anything from fantasy to horror.
   I also own many collections that I have never read, and I plan to read them all someday. Hopefully`,
   
   exit: `If you want to exit my cabin portfolio, just close the tab.`,
